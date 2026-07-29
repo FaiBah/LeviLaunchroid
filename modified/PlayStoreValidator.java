aa27d836659cdeaedfd3eec02b0a1a4080a28995
FB <267238177+FaiBah@users.noreply.github.com>
2026-07-29 22:55:15 +0800

    Update PlayStoreValidator.java

--- modified/PlayStoreValidator.java
+++ modified/PlayStoreValidator.java
@@ -24,3 +24,23 @@ public class PlayStoreValidator {
         return true;
     }
 }
+
+===========================
+
+package org.levimc.launcher.util;
+
+import android.content.Context;
+
+public class PlayStoreValidator {
+    public static boolean isMinecraftFromPlayStore(Context context) {
+        return true;
+    }
+
+    public static boolean isMinecraftInstalled(Context context) {
+        return true;
+    }
+
+    public static boolean isLicenseVerified(Context context) {
+        return true;
+    }
+}
