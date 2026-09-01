bc5c66463df34ccfcbb32b320858605623a58466
FB <267238177+FaiBah@users.noreply.github.com>
2026-09-01 16:16:50 +0800

    Update and rename PlayStoreValidator.java to PlayStoreValidator.java

---
+++ modify/PlayStoreValidator.java
@@ -0,0 +1,17 @@
+package org.levimc.launcher.util;
+
+import android.content.Context;
+
+public class PlayStoreValidator {
+    public static boolean isMinecraftFromPlayStore(Context context) {
+        return true;
+    }
+
+    public static boolean isMinecraftInstalled(Context context) {
+        return true;
+    }
+
+    public static boolean isLicenseVerified(Context context) {
+        return true;
+    }
+}
